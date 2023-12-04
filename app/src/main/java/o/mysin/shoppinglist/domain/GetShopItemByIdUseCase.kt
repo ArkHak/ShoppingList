e87a2eeb9516a939a0817ce43ef3c7b8a891a581
Oleg Mysin <7nacu7@gmail.com>
2023-12-04 21:24:14 +0300

    data, domain, presentation

---
+++ app/src/main/java/o/mysin/shoppinglist/domain/GetShopItemByIdUseCase.kt
@@ -0,0 +1,8 @@
+package o.mysin.shoppinglist.domain
+
+class GetShopItemByIdUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun getShopItem(shopItemId: Int): ShopItem {
+        return shopListRepository.getShopItem(shopItemId)
+    }
+}
--- app/src/main/java/o/mysin/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/o/mysin/shoppinglist/presentation/MainActivity.kt
@@ -1,12 +1,16 @@
-package o.mysin.shoppinglist
+package o.mysin.shoppinglist.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import o.mysin.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
         }
     }
 }
